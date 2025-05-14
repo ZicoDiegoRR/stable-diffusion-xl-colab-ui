23955279f445bc8ac7bb0506da0f08882c16a6ec
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-15 00:50:05 +0700

    Update modified_inference_realesrgan.py

--- modified_realesrgan/modified_inference_realesrgan.py
+++ modified_realesrgan/modified_inference_realesrgan.py
@@ -28,7 +28,7 @@ class ESRGANWidget:
         self.warning_upscale = widgets.HTML(value="It's recommended to upscale any image with up to 1024x1024 resolution or lower to avoid high VRAM usage.")
         self.input_link = widgets.Text(placeholder="Image link or path")
         self.input_upload = widgets.FileUpload(accept="image/*", multiple=False)
-        self.ersgan_input = widgets.HBox([self.warning_upscale, self.input_link, self.input_upload])
+        self.ersgan_input = widgets.VBox([self.warning_upscale, widgets.VBox([self.input_link, self.input_upload])])
         
         self.input_upload.observe(self.input_upload_handler, names="value")
         
@@ -238,4 +238,5 @@ def run_upscaling(
             else:
                 save_path = os.path.join(args.output, f'{imgname}_{args.suffix}.{extension}')
             cv2.imwrite(save_path, output)
-            display(make_image_grid([load_image(input), load_image(save_path)], rows=1, cols=2))
+            input_width, input_height = load_image(input).size
+            display(make_image_grid([load_image(input), load_image(save_path).resize((input_width, input_height))], rows=1, cols=2))
