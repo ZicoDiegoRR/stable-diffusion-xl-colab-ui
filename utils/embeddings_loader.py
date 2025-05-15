f24aa90d9f428187136cf3c598821acd11dd515d
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-15 12:41:14 +0700

    Update embeddings_loader.py

--- utils/embeddings_loader.py
+++ utils/embeddings_loader.py
@@ -5,13 +5,13 @@ import os
 def load_textual_inversion_from_link(pipe, link, token, name):
     # Loading one by one
     for embed, tag, name in zip(link, token, name):
-        ti_dict = load_file(f"embed")
+        dict = load_file(embed)
         try:
             print(f"Loading {name}...")
-            pipe.load_textual_inversion(ti_dict["clip_g"], token=tag, text_encoder=pipeline.text_encoder_2, tokenizer=pipeline.tokenizer_2)
-            pipe.load_textual_inversion(ti_dict["clip_l"], token=tag, text_encoder=pipeline.text_encoder, tokenizer=pipeline.tokenizer)
+            pipe.load_textual_inversion(dict["clip_g"], token=tag, text_encoder=pipe.text_encoder_2, tokenizer=pipe.tokenizer_2)
+            pipe.load_textual_inversion(dict["clip_l"], token=tag, text_encoder=pipe.text_encoder, tokenizer=pipe.tokenizer)
         except Exception as e:
-            print(f"Skipping {name}. Reason: {e}")
+            print(f"Skipped {name}. Reason: {e}")
     
 def download_textual_inversion(pipe, link, token, hf_token, civit_token):
     # Download and handle duplication
@@ -19,7 +19,7 @@ def download_textual_inversion(pipe, link, token, hf_token, civit_token):
     ti_path = []
     unique_ti_urls = []
     
-    for i, url in enumerate(link, start=1):
+    for i, url in enumerate(link):
         if url not in unique_ti_urls:
             if url.startswith("https://") or url.startswith("http://"):
                 textual_inversion_path = downloader.download_file(url, "Embeddings", hf_token, civit_token)
@@ -38,9 +38,9 @@ def download_textual_inversion(pipe, link, token, hf_token, civit_token):
 def process(pipe, link, token, hf_token, civit_token):
     # Preprocessing the urls and weight before downloading
     ti_links = [word for word in re.split(r"\s*,\s*", link)]
-    ti_tokens = [word for word in re.split(r"\s*\s*", link)]
-    if not os.path.exists("/content/Embeddings"):
-        os.makedirs("/content/Embeddings")
+    ti_tokens = [word for word in re.split(r"\s*\s*", token)]
+    
+    os.makedirs("/content/Embeddings", exist_ok=True)
 
     download_textual_inversion(pipe, ti_links, ti_tokens, hf_token, civit_token)
 
