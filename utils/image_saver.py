7d2a1c8f9cee521dd607d3dc5ad79f5319d48d7e
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-15 13:05:32 +0700

    Update image_saver.py

--- utils/image_saver.py
+++ utils/image_saver.py
@@ -15,9 +15,10 @@ def save_image(image, base_path, prompt_for_name, prefix):
         image_save_path = f"{base_path}/Img2Img"
     os.makedirs(image_save_path, exist_ok=True)
   
-    split_prompt = re.split("\s*,\s*", prompt_for_name.replace("<", "").replace(">", "").replace(":", "_").replace(";", "_"))
+    sub_prompt = re.sub(r"[<>:\"/\\|?*]", "_", prompt_for_name)
+    split_prompt = re.split(r"\s*,\s*", sub_prompt)
     prompt_name = " ".join(split_prompt)
-    generated_image_raw_filename = f"{image_save} {formatted_time} {prompt_name}"
+    generated_image_raw_filename = f"{prefix} {formatted_time} {prompt_name}"
     generated_image_filename = generated_image_raw_filename[:251] if len(generated_image_raw_filename) > 255 else generated_image_raw_filename
     generated_image_savefile = f"{image_save_path}/{generated_image_filename}.png"
     image.save(generated_image_savefile)
