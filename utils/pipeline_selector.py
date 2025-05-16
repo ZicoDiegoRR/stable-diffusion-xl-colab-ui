5458c4c5fa68d408b5bb4974cd54a846bedf41d7
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-16 23:00:13 +0700

    Update pipeline_selector.py

--- utils/pipeline_selector.py
+++ utils/pipeline_selector.py
@@ -10,6 +10,7 @@ from diffusers import (
 from StableDiffusionXLColabUI.utils import downloader
 
 def load_pipeline(model_url, format=".safetensors", controlnets=None, active_inpaint=False, vae=None, hf_token, civit_token):
+    # For Hugging Face repository with "author/repo_name" format
     if model_url.count("/") == 1 and (not model_url.startswith("https://") or not model_url.startswith("http://")):
         if all(cn is None for cn in controlnets) and pipeline_type != "img2img" and not active_inpaint:
             pipeline = StableDiffusionXLPipeline.from_pretrained(model_url, vae=vae, torch_dtype=torch.float16).to("cuda")
@@ -19,7 +20,10 @@ def load_pipeline(model_url, format=".safetensors", controlnets=None, active_inp
             pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(model_url, controlnet=[element for element in controlnets if element], vae=vae, torch_dtype=torch.float16).to("cuda")
         elif pipeline_type == "img2img":
             pipeline = StableDiffusionXLImg2ImgPipeline.from_pretrained(model_url, vae=vae, torch_dtype=torch.float16).to("cuda")
+
+    # For non-Hugging Face repository or Hugging Face direct link
     else:
+        # Download
         if model_url.startswith("https://") or model_url.startswith("http://"):
             Model_path = downloader.download_file(model_url, "Checkpoint", hf_token, civit_token)
         else:
@@ -27,6 +31,8 @@ def load_pipeline(model_url, format=".safetensors", controlnets=None, active_inp
                 Model_path = f"/content/Checkpoint/{model_url}.{format}"
             else:
                 Model_path = model_url
+                
+        # Load
         try:
             if all(cn is None for cn in controlnets) and pipeline_type != "img2img" and not active_inpaint:
                 pipeline = StableDiffusionXLPipeline.from_single_file(Model_path, vae=vae, torch_dtype=torch.float16, variant="fp16").to("cuda")
@@ -36,6 +42,8 @@ def load_pipeline(model_url, format=".safetensors", controlnets=None, active_inp
                 pipeline = StableDiffusionXLControlNetPipeline.from_single_file(Model_path, controlnet=[element for element in controlnets if element], vae=vae, torch_dtype=torch.float16, variant="fp16").to("cuda")
             elif pipeline_type == "img2img":
                 pipeline = StableDiffusionXLImg2ImgPipeline.from_single_file(Model_path, vae=vae, torch_dtype=torch.float16, variant="fp16").to("cuda")
+                
+        # Raise an  error if there's something wrong when loading the model
         except (ValueError, OSError):
             if not os.path.exists(Model_path):
                 Error = f"Model {Model_path} doesn't exist."
