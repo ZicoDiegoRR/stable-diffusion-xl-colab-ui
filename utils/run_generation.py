14faba32d759be1ede7800ed7897ec77740df453
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-16 23:27:12 +0700

    Update run_generation.py

--- utils/run_generation.py
+++ utils/run_generation.py
@@ -10,6 +10,7 @@ def generate(
     Clip_Skip,
     generator,
     Inpainting_Strength,
+    IP_Adapter="None",
     image_embeds=None,
     inpaint_image=None,
     mask_image=None,
@@ -18,138 +19,42 @@ def generate(
     ref_image=None,
     Denoising_Strength=None
 ):
+    generation_arguments = {
+        "prompt_embeds": conditioning[0:1],
+        "pooled_prompt_embeds": pooled[0:1],
+        "negative_prompt_embeds": conditioning[1:2],
+        "negative_pooled_prompt_embeds": pooled[1:2],
+        "num_inference_steps": Steps,
+        "width": Width,
+        "height": Height,
+        "guidance_scale": Scale,
+        "clip_skip": Clip_Skip,
+        "generator": generator
+    }
+    if IP_Adapter != "None": # If IP-Adapter is turned on
+        generation_arguments["ip_adapter_image"] = image_embeds
+        
     if pipeline_type == "text2img": # For Text2Img
         image_prefix = "[Text-to-Image]"
-        if IP_Adapter == "None":
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator
-            ).images[0]
-        else:
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                ip_adapter_image=image_embeds,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator
-            ).images[0]
-            pipeline.unload_ip_adapter()
+
     elif pipeline_type == "inpaint": # For Inpainting
         image_prefix = "[Inpainting]"
-        if IP_Adapter == "None":
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                image=inpaint_image,
-                mask_image=mask_image,
-                generator=generator,
-                strength=Inpainting_Strength
-            ).images[0]
-        else:
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                ip_adapter_image=image_embeds,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator,
-                image=inpaint_image,
-                mask_image=mask_image,
-                strength=Inpainting_Strength
-            ).images[0]
-            pipeline.unload_ip_adapter()
+        generation_arguments["image"] = inpaint_image
+        generation_arguments["mask_image"] = mask_image
+        generation_arguments["strength"] = Inpainting_Strength
+
     elif pipeline_type == "controlnet": # For ControlNet
         image_prefix = "[ControlNet]"
-        if IP_Adapter == "None":
-          image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                clip_skip=Clip_Skip,
-                num_inference_steps=Steps,
-                generator=generator,
-                width=Width,
-                height=Height,
-                image=[element for element in images if element is not None],
-                controlnet_conditioning_scale=[element for element in controlnets_scale if element],
-                guidance_scale=Scale
-          ).images[0]
-        else:
-          image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                ip_adapter_image=image_embeds,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator,
-                image=[element for element in images if element is not None],
-                controlnet_conditioning_scale=[element for element in controlnets_scale if element]
-          ).images[0]
+        generation_arguments["image"] = [element for element in images if element is not None]
+        generation_arguments["controlnet_conditioning_scale"] = [element for element in controlnets_scale if element]
+
     else: # For Img2img
         image_prefix = "[Image-to-Image]"
-        if IP_Adapter == "None":
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                width=Width,
-                height=Height,
-                image=ref_image,
-                strength=Denoising_Strength,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator
-            ).images[0]
-        else:
-            image = pipeline(
-                prompt_embeds=conditioning[0:1],
-                pooled_prompt_embeds=pooled[0:1],
-                negative_prompt_embeds=conditioning[1:2],
-                negative_pooled_prompt_embeds=pooled[1:2],
-                num_inference_steps=Steps,
-                ip_adapter_image=image_embeds,
-                image=ref_image,
-                strength=Denoising_Strength,
-                width=Width,
-                height=Height,
-                guidance_scale=Scale,
-                clip_skip=Clip_Skip,
-                generator=generator
-            ).images[0]
-            pipeline.unload_ip_adapter()
+        generation_arguments["image"] = ref_image
+        generation_arguments["strength"] = Denoising_Strength
 
-    return image_prefix
+    image = pipeline(**generation_arguments).images[0]
+    if IP_Adapter != "None": # If IP-Adapter is turned on
+        pipeline.unload_ip_adapter()
+
+    return image_prefix, image
