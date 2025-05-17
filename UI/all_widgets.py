4007d357c669817ac915fe9ed00ac5db7caad82d
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-17 23:39:52 +0700

    Create all_widgets.py

---
+++ UI/all_widgets.py
@@ -0,0 +1,13 @@
+from StableDiffusionXLColabUI.UI.text2img_settings import Text2ImgSettings
+from StableDiffusionXLColabUI.UI.img2img_settings import Img2ImgSettings
+from StableDiffusionXLColabUI.UI.controlnet_settings import ControlNetSettings
+from StableDiffusionXLColabUI.UI.inpainting_settings import InpaintingSettings
+from StableDiffusionXLColabUI.UI.ip_adapter_settings import IPAdapterLoader
+from StableDiffusionXLColabUI.UI.lora_settings import LoRALoader
+from StableDiffusionXLColabUI.UI.textual_inversion_settings import TextualInversionLoader
+
+def import_widgets():
+    widgets_dictionary = {
+        
+    }
+  
