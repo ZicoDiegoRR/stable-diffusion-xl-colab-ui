e7fbc56758539a5008495679bc57078f1c3f0bbf
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-05-17 23:49:29 +0700

    Update all_widgets.py

--- UI/all_widgets.py
+++ UI/all_widgets.py
@@ -1,3 +1,4 @@
+'''
 from StableDiffusionXLColabUI.UI.text2img_settings import Text2ImgSettings
 from StableDiffusionXLColabUI.UI.img2img_settings import Img2ImgSettings
 from StableDiffusionXLColabUI.UI.controlnet_settings import ControlNetSettings
@@ -5,9 +6,17 @@ from StableDiffusionXLColabUI.UI.inpainting_settings import InpaintingSettings
 from StableDiffusionXLColabUI.UI.ip_adapter_settings import IPAdapterLoader
 from StableDiffusionXLColabUI.UI.lora_settings import LoRALoader
 from StableDiffusionXLColabUI.UI.textual_inversion_settings import TextualInversionLoader
+'''
 
-def import_widgets():
+def import_widgets(text2img, img2img, controlnet, inpaint, ip, lora, embeddings):
     widgets_dictionary = {
-        
+        "text2img": text2img.return_widgets(),
+        "img2img": img2img.return_widgets(),
+        "controlnet": controlnet.return_widgets(),
+        "inpaint": inpaint.return_widgets(),
+        "ip": ip.return_widgets(),
+        "lora": lora.return_widgets(),
+        "embeddings": embeddings.return_widgets(),
     }
+    return widgets_dictionary
   
