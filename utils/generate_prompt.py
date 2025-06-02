9ab6c3c56c078dbc4a8f6940cf48e75da7f2c759
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-02 19:38:09 +0700

    Update generate_prompt.py

--- utils/generate_prompt.py
+++ utils/generate_prompt.py
@@ -1,7 +1,7 @@
 from transformers import pipeline as pipe, set_seed
 
 # Prompt wildcard function
-def generate(starting_text, gpt2_pipe, ideas_line):
+def generate(starting_text, ideas_line, gpt2_pipe):
     # Set seed
     random_seed_for_gpt2 = random.randint(100, 1000000)
     set_seed(random_seed_for_gpt2)
