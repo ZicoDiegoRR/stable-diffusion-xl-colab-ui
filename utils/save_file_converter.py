9788107f5efc1a5eaced35bfb38db722465cc9bb
Zico Diego R. R <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-02 17:35:40 +0700

    Update save_file_converter.py

--- utils/save_file_converter.py
+++ utils/save_file_converter.py
@@ -8,29 +8,67 @@ def load_param(filename):
         return params
     except FileNotFoundError:
         return []
+
+def sanitize_none_values(list_value, default_value):
+    sanitized_list = []
+    for i, value in enumerate(list_value):
+        if value is None:
+            sanitized_list.append(default_value[i])
+        else:
+            sanitized_list.append(list_value[i])
+    return sanitized_list
         
 def old_to_new(path):
-    default_param_for_dict = {
-        "text2img": ["", "", "", 1024, 1024, 12, 6, 2, "Default (defaulting to the model)", 
-                     False, False, False, False, "", "",
-                    ],
-        "img2img": ["", "", "", 1024, 1024, 12, 6, 2, "Default (defaulting to the model)", 
-                    False, False, False, False, "", "", "", 0.3,
-                   ],
-        "controlnet": ["", "", "", 1024, 1024, 12, 6, 2, "Default (defaulting to the model)", 
-                      False, False, False, False, "", "", "", 100, 240, False, 
-                      0.7, "", False, 0.7, "", False, 0.7,
-                      ],
-        "inpaint": ["pre-generated text2image image", "", False, 0.9],
-        "ip": ["", 0.8, "None"],
-        "lora": ["", ""],
-        "embeddings": ["", ""],
-    }
+    default_param_for_dict = [
+        "",
+        "",
+        "Safe Tensor (.safetensors)",
+        "",
+        1024,
+        1024,
+        "Default (defaulting to the model)",
+        12,
+        6,
+        "",
+        2,
+        "",
+        "",
+        100,
+        240,
+        "",
+        False,
+        0.7,
+        "",
+        False,
+        0.7,
+        "",
+        False,
+        0.7,
+        "pre-generated text2image image",
+        "",
+        False,
+        0.9,
+        "None",
+        "",
+        0.7,
+        False,
+        False,
+        False,
+        False,
+        False,
+        "",
+        "",
+        0.3,
+        "",
+        "",
+    ]
     
-    target_list = load_param(path)
-    if len(target_list) < 41:
-        for i in range(41 - len(target_list)):
-            target_list.append(None)
+    raw_list = load_param(path)
+    if len(raw_list) < 41:
+        for i in range(41 - len(raw_list)):
+            raw_list.append(None)
+
+    target_list = sanitize_none_values(raw_list, default_param_for_dict)
             
     text2img_or_general_list = [
         target_list[0],
@@ -71,16 +109,39 @@ def old_to_new(path):
     
     inpaint_list = [
         target_list[24],
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
-        target_list,
+        target_list[25],
+        target_list[26],
+        target_list[27],
     ]
+    
+    ip_list = [
+        target_list[29],
+        target_list[30],
+        target_list[28],
+    ]
+    
+    lora_list = [
+        target_list[11],
+        target_list[13],
+    ]
+    
+    ti_list = [
+        target_list[39],
+        target_list[40],
+    ]
+    
+    misc_list = [
+        target_list[31],
+        target_list[2],
+    ]
+
+    new_cfg = {
+        "text2img": text2img_or_general_list,
+        "img2img": text2img_or_general_list + img2img_list,
+        "controlnet": text2img_or_general_list + controlnet_list,
+        "inpaint": inpaint_list,
+        "ip": ip_list,
+        "lora": lora_list,
+        "embeddings": ti_list,
+    }
+    return new_cfg
