f8b0c305de68b8ad7adc89ad6394190cec5a5cee
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-05 17:06:55 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,25 +1,19 @@
 import subprocess
 import os
 
-# Import the preprocess module and UI
-def import_ui():
-    from StableDiffusionXLColabUI.utils import preprocess
-    from StableDiffusionXLColabUI.UI.ui_wrapper import UIWrapper
-
 # Initialize Real-ESRGAN
 def initialize_realesrgan():
     os.chdir("/content/RealESRGAN")
     subprocess.run(["python", "setup.py", "develop"])
     os.chdir("/content")
 
-    import_ui()
-
 # Doing everything
 def start():
     initialize_realesrgan()
+
+    # Import the preprocess module and UI
+    from StableDiffusionXLColabUI.utils import preprocess
+    from StableDiffusionXLColabUI.UI.ui_wrapper import UIWrapper
     
     cfg, ideas_line, gpt2_pipe = preprocess.run() # Preprocess the save file, ideas.txt, and GPT-2
     colab_ui = UIWrapper(cfg, ideas_line, gpt2_pipe) # Initialize the UI
-
-if __name__ == '__main__':
-    start()
