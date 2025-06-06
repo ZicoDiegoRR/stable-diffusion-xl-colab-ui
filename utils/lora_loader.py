3ad4e92c62f7a7a8f678a2b8bdeb5241526491e2
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-06 13:00:54 +0700

    Added a failsafe logic

--- utils/lora_loader.py
+++ utils/lora_loader.py
@@ -21,20 +21,33 @@ def download_lora(pipe, link, scale, widget, hf_token, civit_token):
     unique_lora_urls = []
 
     for i, url in enumerate(link):
+        lora_file_path = ""
         if url not in unique_lora_urls:
             if url.startswith("https://") or url.startswith("http://"):
                 lora_file_path = downloader.download_file(url, "LoRAs", hf_token, civit_token)
             else:
-                lora_file_path = f"/content/LoRAs/{url}"
-            unique_lora_urls.append(url)
-            lora_paths.append(lora_file_path)
+                if url.startswith("/content/LoRAs/"):
+                    lora_check = os.path.basename(url)
+                else:
+                    lora_check = url
+                for lora in os.listdir("/content/LoRAs/"):
+                    if lora_check in lora:
+                        lora_file_path = f"/content/LoRAs/{lora}"
+                        break
+                        
+            if lora_file_path:
+                unique_lora_urls.append(url)
+                lora_paths.append(lora_file_path)
 
-            split_lora_name, _ = os.path.splitext(os.path.basename(lora_file_path))
-            lora_names.append(split_lora_name)
-            scales.append(scale[i])
+                split_lora_name, _ = os.path.splitext(os.path.basename(lora_file_path))
+                lora_names.append(split_lora_name)
+                scales.append(scale[i])
 
-            widget_value = widget.value.replace(url, split_lora_name)
-            widget.value = widget_value
+                widget_value = widget.value.replace(url, split_lora_name)
+                widget.value = widget_value
+            else:
+                print(f"It seems like {url} is an invalid path or doesn't exist. Make sure to put a correct path to ensure the weight being loaded correctly.")
+                print(f"Skipped {url}.")
 
     load_downloaded_lora(pipe, lora_paths, scales, lora_names)
 
@@ -43,4 +56,4 @@ def process(pipe, link, scale, widget, hf_token, civit_token):
     lora_links = re.split(r"\s*,\s*", link)
     lora_scales = [float(num) for num in re.split(r"\s*,\s*", scale)]
     
-    download_lora(pipe, lora_links, lora_scales, hf_token, civit_token)
+    download_lora(pipe, lora_links, lora_scales, widget, hf_token, civit_token)
