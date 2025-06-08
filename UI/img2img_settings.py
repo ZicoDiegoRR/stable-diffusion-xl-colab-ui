35037ed10b5ecb4b8f98cdde6b3dd031f0402a73
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-08 12:49:00 +0700

    Update img2img_settings.py

--- UI/img2img_settings.py
+++ UI/img2img_settings.py
@@ -45,10 +45,11 @@ class Img2ImgSettings:
     def validation(self, value):
         root_dir_list = os.listdir("/content")
         for folder in root_dir_list:
-            file_in_folder = os.listdir(f"/content/{folder}")
-            for file in file_in_folder:
-                if value in file:
-                    return True
+            if os.path.isdir(folder):
+                file_in_folder = os.listdir(f"/content/{folder}")
+                for file in file_in_folder:
+                    if value in file:
+                        return True
         return False
 
     # Check if the path is link
