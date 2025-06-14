f79f19ddf3df066722b0c41af0fd66a3dcc31141
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-14 20:17:04 +0700

    Update pipeline_selector.py

--- utils/pipeline_selector.py
+++ utils/pipeline_selector.py
@@ -31,98 +31,53 @@ def raise_error(Model_path):
         os.remove(Model_path)
     raise TypeError(f"Program quit with an error: {Error}{Warning}")
 
-def flush(pipe, new, old, type):
-    if type == "model":
-        warning_restart = f"You inputted a new model ({new}), which is different than the old one ({old})."
-    elif type == "pipeline":
-        warning_restart = f"You changed the pipeline from {old} to {new}."
-    print(f"{warning_restart} Flushing the model...")
-    del pipe
-    torch.cuda.empty_cache()
-    gc.collect()
-
-def verify(pipe, model_url, loaded_model, loaded_pipeline, pipeline_type):
-    if loaded_model:
-        if loaded_model != model_url:
-            flush(pipe, model_url, loaded_model, "model")
-            model_check = True
-        else:
-            model_check = False
+def load_pipeline(pipe, model_url, widget, pipeline_type, format="safetensors", controlnets=None, active_inpaint=False, vae=None, hf_token="", civit_token="", base_path="/content"):
+    # Download or input the URL or path to urls.json
+    if model_url.startswith(("https://", "http://")) or model_url.count("/"):
+        Model_path = downloader.download_file(model_url, "Checkpoint", hf_token, civit_token, base_path)
     else:
-        model_check = True
-        
-    if loaded_pipeline: 
-        if loaded_pipeline != pipeline_type:
-            flush(pipe, pipeline_type, loaded_pipeline, "pipeline")
-            pipe_check = True
-        else:
-            pipe_check = False
-    else:
-        pipe_check = True
-        
-    if model_check or pipe_check:
-        return True
-    elif not model_check and not pipe_check:
-        return False
-
-def load_pipeline(pipe, model_url, widget, loaded_model, loaded_pipeline, pipeline_type, format="safetensors", controlnets=None, active_inpaint=False, vae=None, hf_token="", civit_token="", base_path="/content"):
-    # Filtering ControlNet's weight if exist
-    if not all(cn is None for cn in controlnets):
-        controlnet_weights = [cn for cn in controlnets if cn]
-        if len(controlnet_weights) == 1:
-            controlnet_weights = controlnet_weights[0]
+        Model_path = url
     
     # For Hugging Face repository with "author/repo_name" format
     if model_url.count("/") == 1 and (not model_url.startswith("https://") or not model_url.startswith("http://")):
-        if verify(pipe, model_url, loaded_model, loaded_pipeline, pipeline_type) or pipe is None:
-            try:
-                if pipeline_type == "text2img" and not active_inpaint:
-                    pipeline = StableDiffusionXLPipeline.from_pretrained(model_url, torch_dtype=torch.float16).to("cuda")
-                elif active_inpaint and pipeline_type == "inpaint":
-                    pipeline = StableDiffusionXLInpaintPipeline.from_pretrained(model_url, torch_dtype=torch.float16).to("cuda")
-                elif pipeline_type == "controlnet":
-                    pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(model_url, controlnet=controlnet_weights, torch_dtype=torch.float16).to("cuda")
-                elif pipeline_type == "img2img":
-                    pipeline = StableDiffusionXLImg2ImgPipeline.from_pretrained(model_url, torch_dtype=torch.float16).to("cuda")
-                model_name = model_url
-            except (ValueError, OSError):
-                raise_error(model_url)
-        else:
-            return pipe, model_url
+        try:
+            if not pipe:
+                pipeline = StableDiffusionXLPipeline.from_pretrained(model_url, torch_dtype=torch.float16).to("cuda")
+            else: 
+                pipeline = pipe
+                
+            if pipeline_type == "text2img" and not active_inpaint and not isinstance(pipeline, StableDiffusionXLPipeline):
+                pipeline = StableDiffusionXLPipeline(**pipeline.components).to("cuda")
+            elif active_inpaint and pipeline_type == "inpaint" and not isinstance(pipeline, StableDiffusionXLInpaintPipeline):
+                pipeline = StableDiffusionXLInpaintPipeline(**pipeline.components).to("cuda")
+            elif pipeline_type == "controlnet" and not isinstance(pipeline, StableDiffusionXLControlNetPipeline):
+                pipeline = StableDiffusionXLControlNetPipeline(**pipeline.components, controlnet=None).to("cuda")
+            elif pipeline_type == "img2img" and  not isinstance(pipeline, StableDiffusionXLImg2ImgPipeline:
+                pipeline = StableDiffusionXLImg2ImgPipeline(**pipeline.components).to("cuda")
+        except (ValueError, OSError):
+            raise_error(model_url)
 
     # For non-Hugging Face repository or Hugging Face direct link
     else:
         os.makedirs("/content/Checkpoint", exist_ok=True)
-        
-        # Download
-        if model_url.startswith("https://") or model_url.startswith("http://"):
-            Model_path = downloader.download_file(model_url, "Checkpoint", hf_token, civit_token, base_path)
-            model_name, _ = os.path.splitext(os.path.basename(Model_path))
-            widget.value = model_name
-        else:
-            if not model_url.startswith("/content/Checkpoint"):
-                Model_path = downloader.download_file(model_url, "Checkpoint", hf_token, civit_token, base_path)
-                model_name, _ = os.path.splitext(os.path.basename(Model_path))
-            else:
-                Model_path = model_url
-                model_name = model_url
+        widget.value, _ = os.path.splitext(os.path.basename(Model_path))
                 
         # Load
-        if verify(pipe, model_url, loaded_model, loaded_pipeline, pipeline_type) or pipe is None:
-            try:
-                if pipeline_type == "text2img" and not active_inpaint:
-                    pipeline = StableDiffusionXLPipeline.from_single_file(Model_path, torch_dtype=torch.float16, variant="fp16").to("cuda")
-                elif active_inpaint and pipeline_type == "inpaint":
-                    pipeline = StableDiffusionXLInpaintPipeline.from_single_file(Model_path, torch_dtype=torch.float16, variant="fp16").to("cuda")
-                elif pipeline_type == "controlnet":
-                    pipeline = StableDiffusionXLControlNetPipeline.from_single_file(Model_path, controlnet=controlnet_weights, torch_dtype=torch.float16, variant="fp16").to("cuda")
-                elif pipeline_type == "img2img":
-                    pipeline = StableDiffusionXLImg2ImgPipeline.from_single_file(Model_path, torch_dtype=torch.float16, variant="fp16").to("cuda")
+        try:
+            if not pipe:
+                pipeline = StableDiffusionXLPipeline.from_single_file(Model_path, torch_dtype=torch.float16).to("cuda")
+            else: 
+                pipeline = pipe
                 
-        # Raise an  error if there's something wrong when loading the model
-            except (ValueError, OSError):
-                raise_error(Model_path)
-        else:
-            return pipe, model_url
+            if pipeline_type == "text2img" and not active_inpaint and not isinstance(pipeline, StableDiffusionXLPipeline):
+                pipeline = StableDiffusionXLPipeline(**pipeline.components).to("cuda")
+            elif active_inpaint and pipeline_type == "inpaint" and not isinstance(pipeline, StableDiffusionXLInpaintPipeline):
+                pipeline = StableDiffusionXLInpaintPipeline(**pipeline.components).to("cuda")
+            elif pipeline_type == "controlnet" and not isinstance(pipeline, StableDiffusionXLControlNetPipeline):
+                pipeline = StableDiffusionXLControlNetPipeline(**pipeline.components, controlnet=None).to("cuda")
+            elif pipeline_type == "img2img" and  not isinstance(pipeline, StableDiffusionXLImg2ImgPipeline:
+                pipeline = StableDiffusionXLImg2ImgPipeline(**pipeline.components).to("cuda")
+        except (ValueError, OSError):
+            raise_error(model_url)
 
-    return pipeline, model_name
+    return pipeline
