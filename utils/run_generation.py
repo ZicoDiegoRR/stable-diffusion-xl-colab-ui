1cb6e336b822d0b31c831e0745cb03f8b8496889
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-16 12:39:11 +0700

    Changed the arguments to lists

--- utils/run_generation.py
+++ utils/run_generation.py
@@ -48,18 +48,8 @@ def generate(
 
     elif pipeline_type == "controlnet": # For ControlNet
         image_prefix = "[ControlNet]"
-        cn_image_filter = [element for element in images if element is not None]
-        if len(cn_image_filter) == 1:
-            cn_image = cn_image_filter[0]
-        else:
-            cn_image = cn_image_filter
-        cn_scale_filter = [element for element in controlnets_scale if element]
-        if len(cn_scale_filter) == 1:
-            cn_scale = cn_scale_filter[0]
-        else:
-            cn_scale = cn_scale_filter
-        generation_arguments["image"] = cn_image
-        generation_arguments["controlnet_conditioning_scale"] = cn_scale
+        generation_arguments["image"] = [element for element in images if element is not None]
+        generation_arguments["controlnet_conditioning_scale"] = [element for element in controlnets_scale if element is not None]
 
     else: # For Img2img
         image_prefix = "[Image-to-Image]"
