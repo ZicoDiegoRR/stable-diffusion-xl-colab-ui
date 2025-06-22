d627bb14ba39d2b769a70b3c5c1cb69b4d125600
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-22 14:57:28 +0700

    Merge option for Inpainting

--- UI/all_widgets.py
+++ UI/all_widgets.py
@@ -1,13 +1,3 @@
-'''
-from StableDiffusionXLColabUI.UI.text2img_settings import Text2ImgSettings
-from StableDiffusionXLColabUI.UI.img2img_settings import Img2ImgSettings
-from StableDiffusionXLColabUI.UI.controlnet_settings import ControlNetSettings
-from StableDiffusionXLColabUI.UI.inpainting_settings import InpaintingSettings
-from StableDiffusionXLColabUI.UI.ip_adapter_settings import IPAdapterLoader
-from StableDiffusionXLColabUI.UI.lora_settings import LoRALoader
-from StableDiffusionXLColabUI.UI.textual_inversion_settings import TextualInversionLoader
-'''
-
 def import_widgets(text2img, img2img, controlnet, inpaint, ip, lora, embeddings):
     widgets_dictionary = {
         "text2img": text2img.return_widgets(),
@@ -32,16 +22,18 @@ def import_values(text2img, img2img, controlnet, inpaint, ip, lora, embeddings):
     }
     return values_dictionary
 
-def merge(init, destination, text2img, img2img, controlnet):
+def merge(init, destination, text2img, img2img, controlnet, inpaint):
     widgets_dictionary_for_merging = {
         "text2img": text2img.return_widgets(),
         "img2img": img2img.return_widgets(),
         "controlnet": controlnet.return_widgets(),
+        "inpaint": inpaint.return_widgets(),
     }
     values_dictionary_for_merging = {
         "text2img": text2img.collect_values(),
         "img2img": img2img.collect_values(),
         "controlnet": controlnet.collect_values(),
+        "inpaint": inpaint.collect_values(),
     }
     init_values = values_dictionary_for_merging[init]
     destination_widgets = widgets_dictionary_for_merging[destination]
