e98b91099fe03a5c751ee20c87fecd89372fdff4
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-24 21:16:35 +0700

    Update ui_wrapper.py

--- UI/ui_wrapper.py
+++ UI/ui_wrapper.py
@@ -4,6 +4,7 @@ from StableDiffusionXLColabUI.UI.controlnet_settings import ControlNetSettings
 from StableDiffusionXLColabUI.UI.inpainting_settings import InpaintingSettings
 from StableDiffusionXLColabUI.UI.ip_adapter_settings import IPAdapterLoader
 from StableDiffusionXLColabUI.UI.lora_settings import LoRALoader
+from StableDiffusionXLColabUI.UI.mask_canvas import MaskCanvas
 from StableDiffusionXLColabUI.UI.textual_inversion_settings import TextualInversionLoader
 from StableDiffusionXLColabUI.UI.reset_and_generate import ResetGenerateSettings
 from StableDiffusionXLColabUI.UI.preset_system import PresetSystem
@@ -11,6 +12,7 @@ from StableDiffusionXLColabUI.UI.history import HistorySystem
 from StableDiffusionXLColabUI.utils import modified_inference_realesrgan, main, downloader
 from StableDiffusionXLColabUI.UI import all_widgets
 from IPython.display import display, clear_output
+from diffusers.utils import load_image
 import ipywidgets as widgets
 import json
 import os
@@ -24,6 +26,57 @@ def load_param(filename):
         return {}
 
 class UIWrapper:
+    # Unused, but could be used for later
+    def get_tab_index(self):
+        return self.ui_tab.selected_index
+
+    def select_class(self, index):
+        if index == 0:
+            return self.text2img
+        elif index == 1:
+            return self.img2img
+        elif index == 2:
+            return self.controlnet
+        elif index == 3:
+            return self.inpaint
+
+    def select_key(self, index):
+        if index == 0:
+            return "text2img" 
+        if index == 1:
+            return "img2img"
+        if index == 2:
+            return "controlnet"
+        if index == 3:
+            return "inpaint"
+
+    # To restart the runtime when users click the "Restart" button
+    def restart(self):
+        os.kill(os.getpid(), 9)
+
+    # Hide the generate and send buttons or show them
+    def checking_the_selected_tab_index(self, change): 
+        self.tab_selected_index = change["new"]
+        if self.tab_selected_index > 3 and self.tab_selected_index!= 7:
+            self.submit_settings.layout.visibility = "hidden"
+            self.merge_options.layout.visibility = "hidden"
+        elif self.tab_selected_index == 7:
+            self.submit_settings.layout.visibility = "visible"
+            self.merge_options.layout.visibility = "hidden"
+        else:
+            self.submit_settings.layout.visibility = "visible"
+            self.merge_options.layout.visibility = "visible"
+
+    # Reload the combobox options
+    def refresh_model(self):
+        saved_models = load_param(f"{self.base_path}/Saved Parameters/URL/urls.json").get("Checkpoint")
+        saved_hf_models = saved_models["hugging_face"] if saved_models and "hugging_face" in saved_models else []
+        if not saved_models:
+            model_options = []
+        else:
+            model_options = list(saved_models["keyname_to_url"].keys())
+        self.model_widget.options = model_options + saved_hf_models
+        
     # Displaying the submit button and resetting the history
     def reload_submit_button(self):
         if not self.is_downloading:
@@ -47,26 +100,6 @@ class UIWrapper:
                 inpainting_list, 
                 upscale_list
             ]
-
-    def select_class(self, index):
-        if index == 0:
-            return self.text2img
-        elif index == 1:
-            return self.img2img
-        elif index == 2:
-            return self.controlnet
-        elif index == 3:
-            return self.inpaint
-
-    def select_key(self, index):
-        if index == 0:
-            return "text2img" 
-        if index == 1:
-            return "img2img"
-        if index == 2:
-            return "controlnet"
-        if index == 3:
-            return "inpaint"
 
     # Running the image generation
     def generate_value(self, index, text2img, img2img, controlnet, inpaint, ip, lora, embeddings):
@@ -119,23 +152,32 @@ class UIWrapper:
             self.upscaler.execute_realesrgan(self.ui)
             self.reload_submit_button()
 
-    # Unused, but could be used for later
-    def get_tab_index(self):
-        return self.ui_tab.selected_index
-
-    # To restart the runtime when users click the "Restart" button
-    def restart(self):
-        os.kill(os.getpid(), 9)
-
-    # Reload the combobox options
-    def refresh_model(self):
-        saved_models = load_param(f"{self.base_path}/Saved Parameters/URL/urls.json").get("Checkpoint")
-        saved_hf_models = saved_models["hugging_face"] if saved_models and "hugging_face" in saved_models else []
-        if not saved_models:
-            model_options = []
+    # Choose the Inpainting settings
+    def inpaint_select(self, mask=False):
+        if not mask:
+            self.inpaint_options.children = [
+                self.inpaint_output,
+                self.model_settings, 
+                widgets.HTML(value="<hr>"), 
+                self.inpaint.wrap_settings(), 
+                self.additional_widgets
+            ]
         else:
-            model_options = list(saved_models["keyname_to_url"].keys())
-        self.model_widget.options = model_options + saved_hf_models
+            self.inpaint_options.children = [self.canvas.wrap_settings()]
+
+    # Create the IPyCanvas
+    def create_mask(self):
+        try:
+            image = load_image(self.inpaint.inpainting_image_dropdown.value)
+            self.canvas.create(image)
+            self.inpaint_select(mask=True)
+        except Exception as e:
+            with self.inpaint_output:
+                print(f"Unable to load the mask image module. Reason: {e}")
+
+    # Return the Inpainting settings back
+    def back_to_inpaint(self):
+        self.inpaint_select()
 
     # Download models from model widget
     def load_model(self, url, hf_token, civit_token, base_path):
@@ -223,18 +265,6 @@ class UIWrapper:
         self.send_controlnet.on_click(lambda b: self.merge_final_phase(type_for_init, "controlnet", index, self.text2img, self.img2img, self.controlnet, self.inpaint))
         self.send_inpaint.on_click(lambda b: self.merge_final_phase(type_for_init, "inpaint", index, self.text2img, self.img2img, self.controlnet, self.inpaint))
         self.merge_back.on_click(lambda b: self.merge_final_phase(type_for_init, "back", index, None, None, None))
-
-    def checking_the_selected_tab_index(self, change): # Hiding the generate and send button or showing them
-        self.tab_selected_index = change["new"]
-        if self.tab_selected_index > 3 and self.tab_selected_index!= 7:
-            self.submit_settings.layout.visibility = "hidden"
-            self.merge_options.layout.visibility = "hidden"
-        elif self.tab_selected_index == 7:
-            self.submit_settings.layout.visibility = "visible"
-            self.merge_options.layout.visibility = "visible"
-        else:
-            self.submit_settings.layout.visibility = "visible"
-            self.merge_options.layout.visibility = "visible"
     
     def __init__(self, cfg, ideas_line, gpt2_pipe, base_path): # cfg as a dictionary
         # Creating the tab
@@ -247,6 +277,7 @@ class UIWrapper:
         self.img2img = Img2ImgSettings(cfg["img2img"], ideas_line, gpt2_pipe)
         self.controlnet = ControlNetSettings(cfg["controlnet"], ideas_line, gpt2_pipe, base_path)
         self.inpaint = InpaintingSettings(cfg["inpaint"], ideas_line, gpt2_pipe)
+        self.canvas = MaskCanvas()
         self.ip = IPAdapterLoader(cfg["ip"])
         self.lora = LoRALoader(cfg["lora"], base_path)
         self.embeddings = TextualInversionLoader(cfg["embeddings"], base_path)
@@ -354,13 +385,21 @@ class UIWrapper:
         # Wrapping additional widgets
         self.additional_widgets = widgets.VBox([self.seed_and_token_section, self.reset_and_send_section])
 
+        # Wrapping the Inpainting
+        self.inpaint_output = widgets.Output()
+        self.inpaint_options = widgets.VBox()
+        self.inpaint_select()
+        
+        self.canvas.back_button.on_click(lambda b: self.back_to_inpaint())
+        self.inpaint.mask_create_button.on_click(lambda b: self.create_mask())
+
         # Creating the UI
         border = widgets.HTML(value="<hr>")
         self.ui_tab.children = [
             widgets.VBox([self.model_settings, border, self.text2img.wrap_settings(), self.additional_widgets]),
             widgets.VBox([self.model_settings, border, self.img2img.wrap_settings(), self.additional_widgets]),
             widgets.VBox([self.model_settings, self.controlnet.wrap_settings(), self.additional_widgets]),
-            widgets.VBox([self.model_settings, border, self.inpaint.wrap_settings(), self.additional_widgets]),
+            ,
             widgets.VBox([self.lora.wrap_settings(), self.token_section]),
             widgets.VBox([self.embeddings.wrap_settings(), self.token_section]),
             self.ip.wrap_settings(),
