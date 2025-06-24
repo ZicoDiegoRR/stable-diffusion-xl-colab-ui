907935352b346ad5f8b6df0e3c8ccc08ea19775b
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-24 14:55:23 +0700

    Update mask_canvas.py

--- UI/mask_canvas.py
+++ UI/mask_canvas.py
@@ -1,30 +1,31 @@
 from IPython.display import display, clear_output
 from ipycanvas import MultiCanvas
 import ipywidgets as widgets
+from io import BytesIO
 from PIL import Image
 
 class MaskCanvas:
     # BRUSH
     #______________________________________________________________________________________________________
-    def brush(x, y):
+    def brush(self, x, y):
         if not self.delete_brush.value:
             self.foreground.fill_style = "white"
         else:
             self.foreground.fill_style = "black"
         self.foreground.fill_circle(x, y, self.brush_size.value)
     
-    def foreground_on_down(x, y):
+    def foreground_on_down(self, x, y):
         self.draw = True
-        brush(x, y)
+        self.brush(x, y)
 
-    def foreground_on_move(x, y):
+    def foreground_on_move(self, x, y):
         if self.draw:
-            brush(x, y)
+            self.brush(x, y)
 
-    def foreground_on_release(x, y):
+    def foreground_on_release(self, x, y):
         self.draw = False
 
-    def brush_preview_move(x, y):
+    def brush_preview_move(self, x, y):
         self.brush_preview.clear()
         self.brush_preview.fill_style = "black"
         self.brush_preview.fill_circle(x, y, self.brush_size.value)
@@ -32,18 +33,27 @@ class MaskCanvas:
 
     # INITIALIZATION
     #______________________________________________________________________________________________________
+    def buffer(self, image):
+        buffer = BytesIO()
+        with buffer:
+            image.save(buffer, format="JPEG")
+            buffer.seek(0)
+            image_io = buffer.read()
+
+        return image_io
+
     def wrap_settings(self):
         return self.canvas_settings
 
     def canvas_setup(self, image):
         img_widget = widgets.Image(
-            value=open(image, "rb").read()
+            value=self.buffer(image)
         )
         self.background.draw_image(img_widget)
         self.background.filter = "brightness(78%) grayscale(5%)"
 
         blank_widget = widgets.Image(
-            value=open(Image.new("RGB", (256, 256), color=(0, 0, 0))).read()
+            value=self.buffer(Image.new("RGB", (self.canvas_width, self.canvas_height), color=(0, 0, 0)))
         )
         self.foreground.draw_image(blank_widget)
         self.foreground.global_alpha = 0.75
@@ -71,11 +81,11 @@ class MaskCanvas:
 
         self.canvas_setup(image)
 
-        self.foreground.on_mouse_down(foreground_on_down)
-        self.foreground.on_mouse_move(foreground_on_move)
-        self.foreground.on_mouse_up(foreground_on_release)
+        self.foreground.on_mouse_down(self.foreground_on_down)
+        self.foreground.on_mouse_move(self.foreground_on_move)
+        self.foreground.on_mouse_up(self.foreground_on_release)
 
-        self.brush_preview.on_mouse_move(brush_preview_move)
+        self.brush_preview.on_mouse_move(self.brush_preview_move)
     
     def __init__(self):
         self.canvas = MultiCanvas(3, width=512, height=512)
@@ -88,7 +98,7 @@ class MaskCanvas:
         self.clear_canvas = widgets.Button(description="❌")
         
         self.submit_button = widgets.Button(description="✅")
-        self.back_button = widgets.Buton(description="🔙")
+        self.back_button = widgets.Button(description="🔙")
 
         self.canvas_settings = widgets.HBox([
             widgets.VBox([
@@ -103,4 +113,3 @@ class MaskCanvas:
                 ]),
             ]),
         ], justify_content="center")
-        
