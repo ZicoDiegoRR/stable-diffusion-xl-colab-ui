5d827aabf55b43b74381507450a37f89e99b8b04
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-29 17:09:51 +0700

    Hires.Fix

---
+++ utils/hires_fix.py
@@ -0,0 +1,21 @@
+from diffusers import StableDiffusionXLImg2ImgPipeline
+from PIL import Image
+import os
+
+def run(pipe, img, hires_values, gen_args): # [lanczos or realesrgan, factor, denoising strength]
+    os.makedirs("/content/hires", exist_ok=True)
+    img.save("/content/hires/temp.png")
+
+    vals = hires_values
+    width, height = img.size
+    if vals[0] == "LANCZOS":
+        image = img.resize((width*vals[1], height*vals[1]), Image.LANCZOS)
+    else:
+        vals[0].hires_execute("/content/hires/temp.png", vals[1])
+        image = Image.open("/content/hires/upscale.png")
+
+    img2img = StableDiffusionXLImg2ImgPipeline(**pipe.components)
+
+    hires_image = img2img(**gen_args, image=image, strength=vals[2]).images[0]
+
+    return hires_image
