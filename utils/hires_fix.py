7736220eab0a0baf24d94914197eb3bb4bdb1680
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-06-30 15:30:00 +0700

    Added a failsafe logic for Hires.Fix

--- utils/hires_fix.py
+++ utils/hires_fix.py
@@ -8,14 +8,19 @@ def run(pipe, img, hires_values, gen_args): # [lanczos or realesrgan, factor, de
 
     vals = hires_values
     width, height = img.size
-    if vals[0] == "LANCZOS":
-        image = img.resize((width*vals[1], height*vals[1]), Image.LANCZOS)
-    else:
-        vals[0].hires_execute("/content/hires/temp.png", vals[1])
-        image = Image.open("/content/hires/upscale.png")
+    try:
+        if vals[0] == "LANCZOS":
+            image = img.resize((width*vals[1], height*vals[1]), Image.LANCZOS)
+        else:
+            vals[0].hires_execute("/content/hires/temp.png", vals[1])
+            image = Image.open("/content/hires/upscale.png")
 
-    img2img = StableDiffusionXLImg2ImgPipeline(**pipe.components)
+        img2img = StableDiffusionXLImg2ImgPipeline(**pipe.components)
 
-    hires_image = img2img(**gen_args, image=image, strength=vals[2]).images[0]
+        hires_image = img2img(**gen_args, image=image, strength=vals[2]).images[0]
+        
+    except Exception as e:
+        print("Couldn't apply Hires.Fix to the image. Returning the original image instead...")
+        return img
 
     return hires_image
