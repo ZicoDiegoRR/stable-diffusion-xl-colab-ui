d14b53665587d1170ea13c7441bc4def7c0507ca
Zico Diego <89088718+ZicoDiegoRR@users.noreply.github.com>
2025-07-19 01:05:24 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -61,7 +61,7 @@ def list_or_dict(cfg, path):
     if isinstance(cfg, list):
         new_cfg = save_file_converter.old_to_new(cfg)
             
-    if isinstance(cfg, dict):
+    if isinstance(new_cfg, dict):
         if len(cfg["inpaint"]) < 19:
             new_cfg = save_file_converter.new_inpaint(cfg)
                 
